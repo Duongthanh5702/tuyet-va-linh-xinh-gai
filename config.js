1eb51f60515828e9df4e1f213857b7ca0056aaff
Yangcheng <71555653+duongdothanh5702@users.noreply.github.com>
2022-10-20 03:58:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const nameGirl = 'Chị Tuyết và Chị Linh';
-const giftUrl = 'http://nodemy.vn';
+const giftUrl = 'https://www.facebook.com/Duongdothanh.5702';
 const eventName = 'Chúc Mừng 20-10';
 const titleCard = 'Chị Tuyết và Chị Linh';
 const contentCard = 'Chúc 2 chị một ngày 20/10 tràn ngập niềm vui và những nụ cười. Mong những điều tuyệt vời nhất sẽ đến với 2 chị trong hôm nay và cả những ngày sau nữa';
